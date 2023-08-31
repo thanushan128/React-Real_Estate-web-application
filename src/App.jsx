753c7d94bde4991bac0166b44ea6a81aba6b7ec5
thanushan128 <thanushanv128@gmail.com>
2023-08-31 10:17:00 +0530

    complete companies component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Header } from "./components/Header/Header";
 import { Hero } from "./components/Hero/Hero";
+import { Companies } from "./components/Companies/Companies";
+
 import './App.css';
 function App() {
   return (
     <div className="App">
       <div>
-        <div className="white-gradient"/>
+        <div className="white-gradient" />
         <Header />
         <Hero />
       </div>
+      <Companies />
     </div>
   );
 }
